12f04b9786c60307ab2ff62326da53e9786c5a87
LucasKeiti28 <lucaskeiti28@gmail.com>
2020-10-21 10:24:25 -0300

    Added script classification algorithm

--- linearRegressionRNN.py
+++ linearRegressionRNN.py
@@ -1,7 +1,10 @@
-import numpy as np
-import matplotlib.pyplot as plt
+# Linear Regression with Tensorflow
+
+# Importing Libs
+import numpy as np 
+import matplotlib.pyplot as plt 
 import tensorflow as tf 
-import os
+import os 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 # Model
@@ -9,70 +12,71 @@ class RegModel:
     def __init__(self):
         self.W = tf.Variable(16.0)
         self.b = tf.Variable(10.0)
-
+    
     def __call__(self, x):
-        return self.W * x + self.b
+        return self.W * x + self.b 
 
-# Defining the true values to Weights and Bias
+# Defining real values to weights and bias
 TRUE_W = 3.0
 TRUE_b = 0.5
 
 # Number of examples
 NUM_EXAMPLES = 1000
 
-# Generate x values
-X = tf.random.normal(shape = (NUM_EXAMPLES,))
+# Generation x values, array with 1-dimension
+X = tf.random.normal(shape = (NUM_EXAMPLES, ))
 
-# Generate noise
-noise = tf.random.normal(shape=(NUM_EXAMPLES,))
+# Generation Noise
+noise = tf.random.normal(shape = (NUM_EXAMPLES,))
 
-# Generate the true value of y
+# Generation the true value of y (tensor with 1-dimension)
 y = X * TRUE_W + TRUE_b + noise
 
-# Calculating the error using cost function
+# Calculate the error using a cost function
 def loss(y, y_pred):
-    return tf.reduce_mean(tf.square(y-y_pred))
+    return tf.reduce_mean(tf.square(y - y_pred))
 
 # Function to execute trainning
 def train(modelo, X, y, lr=0.01):
     with tf.GradientTape() as t:
         current_loss = loss(y, modelo(X))
 
-        derivada_W, derivada_b = t.gradient(current_loss, [modelo.W, modelo.b])
-        modelo.W.assign_sub(lr * derivada_W)
-        modelo.b.assign_sub(lr * derivada_b)
+    # Calculate the deviation using gradient, using cost function
+    derivada_W, derivada_b = t.gradient(current_loss, [modelo.W, modelo.b])
+    modelo.W.assign_sub(lr * derivada_W)
+    modelo.b.assign_sub(lr * derivada_b)
 
-# Creating Model Instance
+# Creating model
 modelo = RegModel()
 
-# Defining empty lists to receive Weights and Bias
+# Defining empty lists to receive W and b
 Ws, bs = [], []
 
-# Epochs how many times the model will pass through the data
+# Number of epochst, how many times the model will pass through data
 epochs = 20
 
-# Trainning
+# Trainning the model
 print("\n")
 for epoch in range(epochs):
-    Ws.append(modelo.W.numpy()) 
+    Ws.append(modelo.W.numpy())
     bs.append(modelo.b.numpy())
 
     current_loss = loss(y, modelo(X))
 
     train(modelo, X, y, lr = 0.1)
-    
+
     print(f"Epoch {epoch}: Loss (Erro): {current_loss.numpy()}")
 
 # Plot
 plt.plot(range(epochs), Ws, 'r', range(epochs), bs, 'b')
 plt.plot([TRUE_W] * epochs, 'r--', [TRUE_b] * epochs, 'b--')
-plt.legend(['W Previsto', 'b Previsto', 'W Real', 'b Real'])
+plt.legend(['W Predicted', 'b Predicted', 'W Real', 'b Real'])
 plt.show()
 
-# Real Value Plotting
+# Ploting real values
 plt.scatter(X, y, label = "Valor Real")
 
-# Predict Value Plotting
+# Plotting predicted values by model trained
 plt.scatter(X, modelo(X), label = "Valor Previsto")
 
 plt.legend()
